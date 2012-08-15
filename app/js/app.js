b7bcf5aaeafd6ccfd39c756c0b8ba67fdb8ef15a
Ricardo Bin <rhsilva@sp.r7.com>
2012-08-15 15:37:31 -0300

    IIFE on base files + jslint

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
-
+(function (angular) {
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'Foo'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'Foo'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+	angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+		config(['$routeProvider', function ($routeProvider) {
+			$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'Foo'});
+			$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'Foo'});
+			$routeProvider.otherwise({redirectTo: '/view1'});
+		}]);
+}(window.angular));
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,11 +1,11 @@
 'use strict';
 
+(function (angular) {
 /* Directives */
-
-
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+	angular.module('myApp.directives', []).
+		directive('appVersion', ['version', function (version) {
+			return function (scope, elm, attrs) {
+				elm.text(version);
+			};
+		}]);
+}(window.angular));
